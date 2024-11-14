a280ab6ba15e2674d81705e3b8c7eb4e21b9d39c
SmakerDS <829shreyashsrivastava@gmail.com>
2024-11-14 10:20:16 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,10 @@
+class student{
+    String name;
+    int id;
+    public static void main(String[]args){
+        student s=new student();
+        s.name="pawan kalyan-bro";
+        s.id=1;
+        System.out.println(s.name+" "+s.id);
+    }
+}
